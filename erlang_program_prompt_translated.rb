0f7dc64a93b128a726e51b95524cce4c9744831e
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2018-02-07 11:22:57 -0800

    Feat: Setup first method to format weight

--- erlang_program_prompt_translated.rb
+++ erlang_program_prompt_translated.rb
@@ -1,6 +1,10 @@
 module RubyProgram
 
   def format_weight(list_of_objects)
+    converted_list = convert_list_to_k(list_of_objects)
+    # print_weight(converted_list)
+    # min_max = find_max_and_min(converted_list)
+    # print_max_and_min(max_object, min_object)
   end
 
   private
